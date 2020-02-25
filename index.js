2a95d62166ec3b506ac4b7c37da3217454bb9472
root <root@Surface-I.localdomain>
2020-02-25 14:11:49 +0100

    Worked on Part Two

--- index.js
+++ index.js
@@ -2,8 +2,30 @@ const express = require('express');
 const app = express();
 const database = require('./public/js/db.js');
 
+///////////////////////////////////////////////////////////////////////////////
+//                    FILE UPLOAD BOILERPLATE CODE                           //
+// /////////////////////////////////////////////////////////////////////////////
+const multer = require('multer');
+const uidSafe = require('uid-safe');
+const path = require('path');
 
+const diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + '/uploads');
+    },
+    filename: function (req, file, callback) {
+        uidSafe(24).then(function(uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    }
+});
 
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152
+    }
+});
 ////////////////////////////////////////////////////////////////////////////////
 //                              STATIC to read HTML                           //
 // /////////////////////////////////////////////////////////////////////////////
@@ -14,7 +36,7 @@ app.use(express.static('./public'));
 ////////////////////////////////////////////////////////////////////////////////
 //                                 GET ROUTES                                 //
 // /////////////////////////////////////////////////////////////////////////////
-app.get('/getImages', (req, res) => {;
+app.get('/getImages', (req, res) => {
     //Database call to get Images URL, Title and Description
     database.getImages().then( results => {
         //setting results.rows to a variable
@@ -23,6 +45,34 @@ app.get('/getImages', (req, res) => {;
         return res.json(imagesData);
     }).catch(err => console.log("Err in getImages() on server side : ", err));
 });
+////////////////////////////////////////////////////////////////////////////////
+//                                 POST ROUTES                                //
+// /////////////////////////////////////////////////////////////////////////////
+app.post('/upload', uploader.single('file'), (req, res) => {
+    console.log("input: ", req.body);
+    console.log("file", req.file);
+
+    if (req.file) {
+        //make database insert HERE
+
+        //Once successful database has been inserted - send to VUE below
+        res.json({
+            success: true
+        });
+    } else {
+        res.json ({
+            success: false
+        });
+    }
+});
+
+
+
+
+
+
+
+
 
 
 
--- public/js/script.js
+++ public/js/script.js
@@ -4,32 +4,48 @@
     new Vue({
         el: "main",
         data: {
-            images: []
+            images: [],
+            //Data Property will store the values of our input fields
+            title: " ",
+            description: " ",
+            username: " ",
+            file: null
         },
         //MOUNTED is a good time to go talk to the server
         mounted: function(){
             console.log("My View Component has mounted!!");
-            // console.log(this.images);
             var me = this;
-            // console.log("this.cities from data: ", this.cities);
             axios.get('/getImages').then( function (response) {
-                // console.log("response from server on the route /getImages: ", response);
-                // console.log(response.data[0].name);
                 //response.data is where the info lives ...
                 me.images = response.data;
-                console.log("Me.images insdie axios is: ", me.images);
-            });
+            }).catch(err => console.log("Err in /getImages in script.js : ", err));
+        }, //Mounted Ends
+        methods: {
+            handleClick: function(e) {
+                e.preventDefault();
+                // console.log("THIS for imageUpload Data: ", this);
+
+                var formData = new FormData();
+                formData.append('title', this.title);
+                formData.append('description', this.description);
+                formData.append('username', this.username);
+                formData.append('file', this.file); 
+
+                axios.post('/upload', formData).then(function(resp){
+                    console.log("resp from POST /upload: ", resp);
+                }).catch(err => console.log("Err in /upload in axios script.js : ", err));
 
 
+            },
+            handleChange: function(e) {
+                //this runs when user select an img in the file input field
+                console.log('HandleChange is running!');
+                console.log('File: ', e.target.files[0]);
 
+                this.file = e.target.files[0];
+                console.log("THIS for imageUpload Data: ", this);
+            }
         }
-        // methods: {
-        //     muffin: function(imageData) {
-        //         console.log("Printing Image Data: ", imageData);
-        //     }
-        // }
-
-
     });
 
 
